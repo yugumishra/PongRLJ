7da71f109ffbe1aaa5aeceb568023a675820a687
BuildTools <unconfigured@null.spigotmc.org>
2025-06-13 03:01:46 -0500

    Policy Gradient Approach (sorta works)

--- src/environment/Pong.java
+++ src/environment/Pong.java
@@ -25,23 +25,17 @@ public class Pong {
 	int numGames;
 	
 	// ---------- ANN variables ---------------
-	public static int MAX_EPISODES = 100_000;
-	public static int datasetSize = 10_000;
+	public static int MAX_EPISODES = 1_000_000;
+	public static int datasetSize = 1_000;
 	public static int STATE_VECTOR_LENGTH = 8;
 	public static int NUM_ACTIONS = 3;
-	public static int HIDDEN_SPACE_LENGTH = 32;
 
 	Tensor states;
-	Tensor predictedQs;
 	int[] actions;
-	Tensor rewards;
-	Tensor nextStates;
-	boolean[] terminals;
 
 	Ann ann;
-	Ann targetNet;
 
-	int networkUpdates = 0;
+	int epochs = 0;
 	int experienceCounter = 0;
 
 	int nnPoints;
@@ -49,7 +43,7 @@ public class Pong {
 
 	float eps = 1.0f;
 	float epsDecay = 0.99f;
-	float gamma = 0.99f;
+	float gamma = 0.95f;
 
 	Optimizer optimizer;
 	Metrics tracker;
@@ -121,52 +115,30 @@ public class Pong {
 			divider[i] = d;
 		}
 		
-		ballXVelocity = -0.02f;
-		ballYVelocity =  0.0f;
+		resetGame();
 		
 		//init anns
 		Input input = new Input(new Shape(STATE_VECTOR_LENGTH));
-		
-		Dense hidden1 = new Dense(input, new Shape(HIDDEN_SPACE_LENGTH), false);
-		Activation activation1 = new Activation(hidden1, ActivationFunction.RELU);
-		Dropout drop1 = new Dropout(activation1, 0.2f);
 
-		Dense hidden2 = new Dense(drop1, new Shape(HIDDEN_SPACE_LENGTH), false);
-		Activation activation2 = new Activation(hidden2, ActivationFunction.RELU);
-		Dropout drop2 = new Dropout(activation2, 0.5f);
+		Dense hidden = new Dense(input, new Shape(STATE_VECTOR_LENGTH), true);
 		
-		Dense output = new Dense(drop2, new Shape(NUM_ACTIONS), true);
+		Dense lay = new Dense(hidden, new Shape(NUM_ACTIONS), false);
+		Activation output = new Activation(lay, ActivationFunction.SOFTMAX);
 		ann = new Ann(input, output);
 		
 		ann.printSummary();
-		
-		//send 0s through the network for initialization
-		Tensor t = new Tensor(new Shape(8, STATE_VECTOR_LENGTH));
-		t.init();
-		ann.predict(t);
 
 		//initialize optimizer and tracker
-		optimizer = new AdamOptimizer(3e-4f, 0.995f, 0.9f, 0.999f, 0.0f);
-		tracker = new ann.Metrics(true, false, true);
-
-		//save network into target file, then read it for the target network
-		
-		ann.save("targetNet_" + networkUpdates + ".bin");
-		targetNet = Ann.load("targetNet_"+ networkUpdates + ".bin");
+		//lr = 3e-4
+		optimizer = new AdamOptimizer(1f, 0.9f, 0.999f);
+		//track loss, don't track accuracy, and print it
+		tracker = new ann.Metrics(false, false, true);
 
 		//initialize storage
 		states = new Tensor(new Shape(datasetSize, STATE_VECTOR_LENGTH));
-		predictedQs = new Tensor(new Shape(datasetSize, NUM_ACTIONS));
-		nextStates = new Tensor(new Shape(datasetSize, STATE_VECTOR_LENGTH));
 		states.init();
-		predictedQs.init();
-		nextStates.init();
-
-		rewards = new Tensor(new Shape(datasetSize));
-		rewards.init();
 
 		actions = new int[datasetSize];
-		terminals = new boolean[datasetSize];
 
 		nnPoints = 0;
 		totPoints = 0;
@@ -195,31 +167,40 @@ public class Pong {
 		board2.cleanup();
 		ball.cleanup();
 		for(Drawable d: divider) d.cleanup();
+
+		ann.save("final");
 	}
 
 	public void update() {
+		//get current state from game
 		Tensor state = getState();
+		//propagate through ann and select from the probability distribution outputted
 		int action = getAnnAction(state);
+		//get idx (explained in the below comment)
+		int idx = experienceCounter % datasetSize;
 		//update buffers
 		for(int i= 0; i< STATE_VECTOR_LENGTH; i++) {
-			states.set(STATE_VECTOR_LENGTH * experienceCounter + i, state.at(i));
+			//here we overwrite the oldest entries by modding the index by datasetSize
+			//then, in training time, we can rewind from here for discounting
+			states.set(STATE_VECTOR_LENGTH * idx + i, state.at(i));
 		}
-		actions[experienceCounter] = action;
+		//similarly overwrite oldest action here
+		actions[idx] = action;
 
-	  if(action == 0 || w.getKey(GLFW.GLFW_KEY_W)) { 
-	      board1.yDisplacement += 0.03f;
-	      board1Velocity = 0.03f;
-	      
-	  } else if(action == 1 || w.getKey(GLFW.GLFW_KEY_S)) { 
-	      board1.yDisplacement -= 0.03f;
-	      board1Velocity = 0.03f;
-	      
-	  }
+		//now propagate action to forward movement
+		if(action == 0) {
+			board1Velocity = 0.05f;
+		}else if(action == 1) {
+			board1Velocity = -0.05f;
+		}else{
+			board1Velocity = 0.0f;
+		}
+		board1.yDisplacement += board1Velocity;
 	    
 	  //simple board2 policy
 	  float displacement = 0.075f * (ball.yDisplacement - board2.yDisplacement);
 	  board2Velocity = Math.signum(displacement) * Math.min(Math.abs(displacement), 0.03f);
-	  board2.yDisplacement = board2.yDisplacement + board2Velocity;
+	  board2.yDisplacement += board2Velocity;
 	    
 	    
 		//propagating ann action to next state
@@ -233,44 +214,23 @@ public class Pong {
 		ball.xDisplacement += ballXVelocity;
 		ball.yDisplacement += ballYVelocity;
 
-		//get next state and update buffer
-		Tensor nextState = getState();
-		for(int i =0 ; i< STATE_VECTOR_LENGTH; i++) {
-			nextStates.set(STATE_VECTOR_LENGTH * experienceCounter + i, nextState.at(i));
-		}
-
-		//calculate reward
-		float reward = 0.0f;
-
-		//reward based on paddle/ball estimation
-		float timeToReachPaddle = (board1.xDisplacement - ball.xDisplacement) / ballXVelocity;
-		float predictedY = ball.yDisplacement + ballYVelocity * timeToReachPaddle;
-
-		if (predictedY - 0.025f < -1.0f) predictedY = -2.0f - predictedY;
-		if (predictedY + 0.025f >  1.0f) predictedY =  2.0f - predictedY;
-
-		//negative award for being away
-		float dist = Math.abs(board1.yDisplacement - predictedY);
-		if(dist > 0.13f) {
-			reward -= dist;
-		}
-
-		
-		//check with board 1
+		//check for collision with board 1
 		if(ball.xDisplacement - 0.025f < board1.xDisplacement + 0.025f && Math.abs(ball.yDisplacement - board1.yDisplacement) < 0.15f && ball.xDisplacement - 0.025f > board1.xDisplacement - 0.025f) {
+				//set to border and flip velocity
 				ball.xDisplacement = board1.xDisplacement + 0.05f;
 				ballXVelocity *= -1;
 				
 				//calculate hit pos relative to the center (and adjust the return y velocity based on that)
 				float hitPos = (ball.yDisplacement - board1.yDisplacement) / 0.15f;
 				ballYVelocity = hitPos * 0.03f; //dummy factor (can change)
-				//ball was hit, increase reward
-				reward += 0.1f;
 				
 				//slightly increase ball speed
 				ballXVelocity *= 1.01f;
 				ballYVelocity *= 1.01f;
+				
+				//board 2 next
 		} else if(ball.xDisplacement + 0.025f > board2.xDisplacement - 0.025f && Math.abs(ball.yDisplacement - board2.yDisplacement) < 0.15f && ball.xDisplacement + 0.025f < board2.xDisplacement + 0.025f) {
+				//set to paddle border and flip velocity
 				ball.xDisplacement = board2.xDisplacement - 0.05f;
 				ballXVelocity *= -1;
 				
@@ -285,6 +245,7 @@ public class Pong {
 		
 		//check for ball collision with top & bottom walls
 		if(ball.yDisplacement + 0.025f > 1.0f || ball.yDisplacement - 0.025f < -1.0f) {
+				//flip velocity
 				ballYVelocity *= -1;
 		}
 		
@@ -297,12 +258,6 @@ public class Pong {
 			//point ball towards who lost
 			ballXVelocity = -0.02f;
 
-			//lost the ball, penalty
-			reward -= 1;
-
-			//terminal state, mark
-			terminals[experienceCounter] = true;
-
 			totPoints++;
 		}else if(ball.xDisplacement - 0.025f > 1f && frameCounter == 50) {
 			//player two lost
@@ -310,11 +265,6 @@ public class Pong {
 			somebodyLost = true;
 			//point ball towards who lost
 			ballXVelocity = 0.02f;
-
-			//got the point, reward
-			reward += 1;
-
-			terminals[experienceCounter] = true;
 
 			nnPoints++;
 			totPoints++;
@@ -332,85 +282,66 @@ public class Pong {
 			ball.yDisplacement = (float) (2 * Math.random() - 1);
 			ball.xDisplacement = 0;
 			ballYVelocity = (Math.random() > 0.5) ? (0.01f) : (-0.01f);
-		}
+			ballXVelocity = Math.signum((float) Math.random() - 0.05f) * 0.02f;
 
-		//place into buffer
-		rewards.set(experienceCounter, reward);
-
-		//train
-		if(experienceCounter == datasetSize-1) {
-			long diff = System.currentTimeMillis();
-			//calculate target q values
-			Tensor nextQs = targetNet.predict(nextStates);
-
-			float[] maxNQs = new float[datasetSize];
-			for(int i =0; i< datasetSize; i++) {
-				float max = -Float.MAX_VALUE;
-				for(int j = 0; j< NUM_ACTIONS; j++) {
-					max = Math.max(max, nextQs.at(i*NUM_ACTIONS + j));
-				}
-				maxNQs[i] = max;
-			}
-
-			Tensor targetQs = new Tensor(new Shape(datasetSize, NUM_ACTIONS));
-			targetQs.init();
-			for(int i =0; i< datasetSize; i++) {
-				float qup = rewards.at(i);
-				if(!terminals[i]) {
-					qup += gamma * maxNQs[i];
-				}
-
-				for(int j =0; j< NUM_ACTIONS; j++) {
-					if(j == actions[i]) {
-						targetQs.set(i*NUM_ACTIONS + j, qup);
-					}else {
-						targetQs.set(i*NUM_ACTIONS + j, predictedQs.at(i*NUM_ACTIONS + j));
-					}
-				}
-			}
-
-			//train
-			Tensor[] dataset = {states, targetQs};
-			ann.train(dataset, 1, 64, false, optimizer, tracker);
-			//decay eps here
-			eps *= epsDecay;
-			//max it out at 5% of actions are random
-			eps = Math.max(eps, 0.05f);
-
-			experienceCounter = 0;
-			//reset storages
-			states.init();
-			predictedQs.init();
-			nextStates.init();
-			rewards.init();
-			actions = new int[datasetSize];
-			terminals = new boolean[datasetSize];
-			
-			diff = System.currentTimeMillis() - diff;
-			System.out.println("Epoch " + (networkUpdates+1) + " took " + (((int) diff) / 1000.0f) + " seconds.");
-			//print amount of points scored by nn (game metric)
-			if(fast) System.out.println("NN scored " + (100 * ((float) nnPoints / (float) totPoints)) + "% of points.");
-			nnPoints = 0;
-			totPoints = 0;
-
-			networkUpdates++;
-			if(networkUpdates % 10 == 0) {
-				//update target network
-				ann.save("targetNet_" + networkUpdates + ".bin");
-				targetNet = Ann.load("targetNet_"+ networkUpdates + ".bin");
-			}
-
-			if(networkUpdates % 100 == 0) {
-				fast = false;
-			}else {
-				fast = true;
-			}
+			//set ys to 0
+			board1.yDisplacement = 0.0f;
+			board2.yDisplacement = 0.0f;
 		}
 		
 		if(playerOnePoints > 10 || playerTwoPoints > 10) {
 			//game over
 			numGames++;
 			resetGame();
+
+			//create custom scale tensor
+			//this is gamma discounting each example
+			//since we want further away actions to be weighted less, we will go from idx and work backwards (loop around 0 if necessary)
+
+			//use broadcasting, so only need one scalar per example (datasetSize examples)
+			Tensor customScale = new Tensor(new Shape(1, datasetSize));
+			customScale.init();
+
+			for(int i = 0; i < datasetSize; i++) {
+				int mapped = (idx - i + datasetSize) % datasetSize;
+				float discount = (float) (Math.pow(gamma, i));
+				customScale.set(mapped, discount);
+			}
+
+			//update optimizer object
+			optimizer.customScale(customScale);
+			//positive=true means reinforce (make probability of this happening higher), positive=false means discourage (make probability of this happening lower)
+			//however gradient direction is opposite (descent = make prob higher, ascent = make prob lower), so switch sign
+			optimizer.setGradientDirection(playerTwoPoints == 11);
+
+			//train!!
+			Tensor[] dataset = {states, oneHot(actions, NUM_ACTIONS)};
+			ann.train(dataset, 1, 50, false, optimizer, tracker);
+
+			//decay epsilon
+			if(epochs % 50 == 0) eps *= epsDecay;
+			//minimum 5% of actions are random
+			eps = Math.max(eps, (epochs < 10_000) ? 0.05f : 0.0f);
+
+			//set experience counter back to 0 and reset storages
+			experienceCounter = 0;
+
+			states.init();
+			actions = new int[datasetSize];
+
+			//epoch done, increment counter
+			epochs++;
+
+			nnPoints = 0;
+			totPoints = 0;
+			
+			
+			//save if % 10
+			if(epochs % 10000 == 0) {
+				ann.save("targetNet_" + (epochs/50000) + ".bin");
+			}
+
+			fast = !(epochs % 10000 == 0);
 		}
 
 		experienceCounter++;
@@ -419,14 +350,18 @@ public class Pong {
 	public void resetGame() {
 		board1.yDisplacement = 0;
 		board2.yDisplacement = 0;
-		ball.yDisplacement = 0;
 		ball.xDisplacement = 0;
-		ballXVelocity = -0.01f;
-		ballYVelocity = 0;
+		//reset ball position to random y & random velocity
+		ball.yDisplacement = (float) (2 * Math.random() - 1);
+		ball.xDisplacement = 0;
+		ballYVelocity = (Math.random() > 0.5) ? (0.01f) : (-0.01f);
+		ballXVelocity = Math.signum((float) Math.random() - 0.05f) * 0.02f;
 		board1Velocity = 0.0f;
 		board2Velocity = 0.0f;
 		frameCounter = 50;
-		if(!fast) System.out.println("Game " + numGames + ": Player 1: "+ playerOnePoints + ", Player 2: " + playerTwoPoints);
+		if(fast && epochs % 1000 == 0) {
+			System.out.println("Game " + numGames + ": Player 1: "+ playerOnePoints + ", Player 2: " + playerTwoPoints + "\t Randomness: " + 100.0f * eps);
+		}
 		playerOnePoints = 0;
 		playerTwoPoints = 0;
 	}
@@ -434,45 +369,45 @@ public class Pong {
 	//-------------ANN stuff ---------------------------
 	
 	public int getAnnAction(Tensor state) {
-		//forward prop through ann
-		Tensor expected = ann.predict(state);
-		//plop into buffer
-		for(int i = 0; i< NUM_ACTIONS; i++) {
-			predictedQs.set(experienceCounter * NUM_ACTIONS + i, expected.at(i));
-		}
+	// Forward pass
+	Tensor probs = ann.predict(state); //shape = (NUM_ACTIONS)
 
-		//epsilon-greedy selection
-		double rand = Math.random();
-		if(rand < eps) {
-			if(rand < 0.333333f) {
-				return 0;
-			}
-			if(rand < 0.66667f) {
-				return 1;
-			}
-			return -1;
-		}else {
-			int max = 0;
-			for(int i = 1; i< NUM_ACTIONS; i++) {
-				if(expected.at(i) > expected.at(max)) {
-					max = i;
-				}
-			}
-			return max;
+	//epsilon greedy, explore based on epsilon value
+	if (Math.random() < eps) {
+		return (int)(Math.random() * NUM_ACTIONS);//choose a random action
+	}
+
+	//sample from the probability distribution outputted by the network
+	double r = Math.random();
+	double cumulative = 0.0;
+	for (int i = 0; i < NUM_ACTIONS; i++) {
+		cumulative += probs.at(i);
+		if (r < cumulative) {
+			return i; //select action
 		}
 	}
-	
-	public Tensor getInitialState() {
-		float[] state = {
-			0.0f, 0.0f, //initial ball displacement	
-			-0.01f, 0.0f, //initial ball velocity
-			
-			0.0f, 0.0f,//initial paddle 1 y displacement + velocity
-			0.0f, 0.0f // initial paddle 2 y displacement + velocity
-		};
-		Tensor t = new Tensor(new Shape (STATE_VECTOR_LENGTH));
-		t.init(state);
-		return t;
+
+	//choose standing still by def
+	return NUM_ACTIONS - 1;
+}
+
+
+	public Tensor oneHot(int action, int numActions) {
+		Tensor res = new Tensor(new Shape(numActions));
+		res.init();
+		res.set(action, 1.0f);
+		return res;
+	}
+
+	//up but batched!!
+	public Tensor oneHot(int[] action, int numActions) {
+		Tensor res = new Tensor(new Shape(action.length, numActions));
+		res.init();
+		for(int i =0 ; i< action.length; i++) {
+			res.set(i * numActions + action[i], 1.0f);
+		}
+
+		return res;
 	}
 	
 	public Tensor getState() {
@@ -480,8 +415,8 @@ public class Pong {
 			ball.xDisplacement, ball.yDisplacement, //ball displacement	
 			ballXVelocity, ballYVelocity, //ball velocity
 			
-			board1.yDisplacement, board1Velocity, //paddle 1 y displacement + velocity
-			board2.yDisplacement, board2Velocity // paddle 2 y displacement + velocity
+			board1.yDisplacement, board1Velocity, //paddle 1 y displacement
+			board2.yDisplacement, board2Velocity // paddle 2 y displacement
 		};
 		Tensor t = new Tensor(new Shape (STATE_VECTOR_LENGTH));
 		t.init(state);
